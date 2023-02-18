bd3e1d930d549c26500de39e31c858356500ee76
coderseyfi <seyfaddinmn@code.edu.az>
2023-02-19 01:00:25 +0400

    chat/SignalR

--- Cozy.Domain/AppCode/Hubs/ChatHub.cs
+++ Cozy.Domain/AppCode/Hubs/ChatHub.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.SignalR;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Concurrent;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Cozy.Domain.AppCode.Hubs
@@ -100,33 +101,84 @@ namespace Cozy.Domain.AppCode.Hubs
 
     public class ChatHub : Hub
     {
-        public override Task OnConnectedAsync()
+        private readonly IMediator mediator;
+        static ConcurrentDictionary<int, string> clients = new ConcurrentDictionary<int, string>();
+
+        public ChatHub(IMediator mediator)
         {
-            return base.OnConnectedAsync();
+            this.mediator = mediator;
         }
 
-        
+        public override async Task OnConnectedAsync()
+        {
+            await base.OnConnectedAsync();
+
+            var httpContext = Context.GetHttpContext();
+            if (httpContext.User.Identity.IsAuthenticated)
+            {
+                var userId = httpContext.User.GetCurrentUserId();
+                clients.AddOrUpdate(userId, Context.ConnectionId, (k, v) => v);
+
+                var command = new HubUserConnectCommand
+                {
+                    HubContext = Context,
+                    HubGroups = Groups
+                };
+
+                await mediator.Send(command);
+            }
+
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+
+            return base.OnDisconnectedAsync(exception);
+        }
+
 
         //istifadeci gonderir mesaj operatorlara gedir
         public async Task SendMessage(string message)
         {
+            var command = new SendToGroupCommand
+            {
+                HubContext = Context,
+                GroupName = "CallCenter",
+                Message = message
+            };
+
+            var response = await mediator.Send(command);
+
             var httpContext = Context.GetHttpContext();
             var userId = httpContext.User.GetCurrentUserId();
 
             //save to db
-            await Clients.Group("CallCenter").SendAsync("ReceiveFromOperator", userId, message);
 
+            await Clients.GroupExcept(command.GroupName, Context.ConnectionId)
+                .SendAsync("ReceiveFromOperator", JsonConvert.SerializeObject(response));
         }
 
-        //operator gonderir istifadeciye gedir
-        public async Task SendFromOperator(string message)
+
+        //operator gonderir mesaj istifadecilere gedir
+        public async Task SendFromOperator(string message, int toUserId)
         {
             var httpContext = Context.GetHttpContext();
             var userId = httpContext.User.GetCurrentUserId();
 
-            //save to db
-            await Clients.All.SendAsync("ReceiveFromClient", userId, message);
+            var command = new SendToUserCommand
+            {
+                HubContext = Context,
+                UserId = toUserId,
+                Message = message
+            };
 
+            var response = await mediator.Send(command);
+
+            if (response != null && clients.TryGetValue(response.ToId.Value, out string toUserConnectionId))
+            {
+                await Clients.User(toUserConnectionId)
+                    .SendAsync("ReceiveFromClient", response.ToId.Value, response.Text);
+            }
         }
     }
 
---
+++ Cozy.WebUI/wwwroot/libs/live-chat/chat.js
@@ -0,0 +1,61 @@
+﻿function chatBox() {
+    this.context = {
+        inbox: [
+            //   {
+            //     FriendId: 0,
+            //     FriendName: "Demo",
+            //     LastMessage: "bye",
+            //     unread: 3,
+            //     date: "2022-11-12 12:44",
+            //   },
+        ],
+    };
+
+    this.receiveMessage = (inboxMessage) => {
+        let anotherMessages = this.context.inbox.filter(
+            (i) => i.FriendId != inboxMessage.FriendId
+        );
+
+        anotherMessages.unshift(inboxMessage);
+        this.context.inbox = anotherMessages;
+
+        console.log(this.context.inbox);
+
+        this.reloadInbox();
+    }
+
+    this.reloadInbox = () => {
+        let messageList = document.querySelector("#messagelist");
+
+        console.log(messageList);
+
+        if (messageList == undefined) return;
+
+        messageList.innerHTML = "";
+
+        this.context.inbox.forEach((item) => {
+            console.log(item);
+            let div = document.createElement("div");
+            div.className = "media userlist-box";
+            div.setAttribute("data-id", item.FriendId);
+            div.setAttribute("title", item.FriendName);
+
+            let a = document.createElement('a');
+            a.className = 'media-left';
+            a.innerHTML = `<img class="media-object img-radius img-radius" src="/admin/images/avatar-3.jpg" alt="Generic placeholder image ">
+      <div class="live-status bg-success"></div>`;
+
+            div.appendChild(a);
+
+            let divMediaBody = document.createElement('div');
+            divMediaBody.className = 'media-body';
+            divMediaBody.innerHTML = `<div class="f-13 chat-header d-flex flex-column">${item.FriendName}
+                <span>${item.LastMessage}</span>
+                </div>`;
+
+            div.appendChild(divMediaBody);
+
+            messageList.appendChild(div);
+        });
+    }
+}
